9f5a19a745300e1e7eea6161f4908abba2cd4b09
Wasif Iqbal <wasifiqbal89@gmail.com>
2022-05-29 14:08:16 +0500

    Add Todo Edit/Add Component [ToBeContinued]

--- src/Blazor/Learn.BlazorServerApp/Models/TodoItem.cs
+++ src/Blazor/Learn.BlazorServerApp/Models/TodoItem.cs
@@ -1,10 +1,18 @@
-﻿namespace Learn.BlazorServerApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Learn.BlazorServerApp.Models
 {
     public class TodoItem
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public bool IsCompleted { get; set; }
+        [Required]
+        public string Title { get; set; } = String.Empty;
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        [Required]
+        public DateTime ScheduledDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public DateTime CompletedDate { get; set; }
+        
+        public bool IsCompleted { get; set; } = false;
     }
 }
--- src/Blazor/Learn.BlazorServerApp/Services/ToDoService.cs
+++ src/Blazor/Learn.BlazorServerApp/Services/ToDoService.cs
@@ -4,15 +4,45 @@ namespace Learn.BlazorServerApp.Services
 {
     public class ToDoService
     {
-        public Task<List<TodoItem>> GetList()
+        private List<TodoItem> _TodoItems;
+        public ToDoService()
         {
-            return Task.FromResult(new List<TodoItem>
+            _TodoItems = new List<TodoItem>
             {
                 new TodoItem(){ Id = 1, Title = "Breakfast", CreatedDate = DateTime.Now, IsCompleted =false },
                 new TodoItem(){ Id = 2, Title = "Shoping", CreatedDate = DateTime.Now, IsCompleted =false },
                 new TodoItem(){ Id = 3, Title = "Cleaning", CreatedDate = DateTime.Now, IsCompleted =false },
                 new TodoItem(){ Id = 4, Title = "Car Wash", CreatedDate = DateTime.Now, IsCompleted =false },
-            });
+            };
+        }
+        public Task<List<TodoItem>> GetList()
+        {
+            return Task.FromResult(_TodoItems);
+        }
+
+        public void Add(TodoItem item)
+        {
+            item.Id = _TodoItems.Max(x => x.Id) + 1;
+            this._TodoItems.Add(item);
+        }
+
+        public void Update(TodoItem item)
+        {
+            var itemRecord = _TodoItems.Where(x=>x.Id == item.Id).FirstOrDefault();
+            if(itemRecord != null)
+            {
+            }
+
+        }
+
+        public TodoItem Get(int itemId)
+        {
+            return this._TodoItems.Where(x=>x.Id == itemId).First();
+        }
+
+        public bool Delete(int itemId)
+        {
+            return this._TodoItems.Remove(this.Get(itemId));
         }
     }
 }
